8ce474c88a78b8f63e579a26a2acae9eceb8f710
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2018-10-08 12:58:43 +0200

    applies prettier formatting to tests

--- test/utils.test.js
+++ test/utils.test.js
@@ -45,7 +45,9 @@ test('`addFilenameToEntries()` adds `filename` prop to each entry', (t) => {
   const entriesCount = 2;
   t.plan(entriesCount * 2);
 
-  const items = Array(entriesCount).fill('a').map(() => ({ entry: { foo: 'bar' } }));
+  const items = Array(entriesCount)
+    .fill('a')
+    .map(() => ({ entry: { foo: 'bar' } }));
 
   const result = utils.addFilenameToEntries(items);
 
@@ -55,8 +57,11 @@ test('`addFilenameToEntries()` adds `filename` prop to each entry', (t) => {
   });
 });
 
-test.todo('`writeEntryResponseBodiesToTargetDir()` writes contents of each entry\'s buffer to `targetDir`');
+test.todo(
+  "`writeEntryResponseBodiesToTargetDir()` writes contents of each entry's buffer to `targetDir`"
+);
 
+// eslint-disable-next-line no-unused-vars
 test.skip('`readHARFromFile()` reads har content from file', (t) => {
   // returns promise
   // rejects when file error
@@ -67,19 +72,18 @@ test.skip('`readHARFromFile()` reads har content from file', (t) => {
 
 test.todo('`splitResponseBodyFromEntry()` creates buffer with response content for each entry');
 
-test.todo('`splitResponseBodyFromEntries()` invokes `splitResponseBodyFromEntry()` for each log.entry.response');
+test.todo(
+  '`splitResponseBodyFromEntries()` invokes `splitResponseBodyFromEntry()` for each log.entry.response'
+);
 
-test.todo('`replaceResponseContentTextWithFilename()` clones entry and puts `filename` into clone.response.content.text');
+test.todo(
+  '`replaceResponseContentTextWithFilename()` clones entry and puts `filename` into clone.response.content.text'
+);
 
-test.todo('`replaceResponseContentTextWithFilenames()` incokes `replaceResponseContentTextWithFilename()` for each item');
+test.todo(
+  '`replaceResponseContentTextWithFilenames()` incokes `replaceResponseContentTextWithFilename()` for each item'
+);
 
 test.todo('`cloneHARAndReplaceEntries()` clones har object and puts replaces entries in clone');
 
 test.todo('`writeHARToFile()` stringifies har object and writes the contents to file');
-
-// (() => {
-//   const client = httpClient();
-//   ['jar', 'use', 'makeRequest', 'get', 'put', 'post', 'del', 'head', 'options'].forEach(methodName =>
-//     test(`client instance exposes method '${methodName}'`, t =>
-//       t.true(_.isFunction(client[methodName]), `client should have "${methodName}" method`)));
-// })();
