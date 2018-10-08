f218400745b0d634f5d5cba5a2d8f6f67b4c9675
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2018-10-08 13:53:42 +0200

    adds unit test for inputFile nested in outputDir

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,5 @@
 // node core modules
+import path from 'path';
 
 // 3rd party modules
 import test from 'ava';
@@ -9,9 +10,32 @@ import rimraf from 'rimraf';
 import { uuid } from '../lib/utils';
 import harExtract from '../lib';
 
+const promisedRimraf = dir => new Promise((resolve, reject) => {
+  rimraf(dir, (rimrafError) => {
+    if (rimrafError) {
+      reject(rimrafError);
+      return;
+    }
+
+    resolve();
+  });
+});
+
 test('main export is a function', (t) => {
   t.is(typeof harExtract, 'function', 'should be of type function');
 });
+
+test('inputFile located inside outputDir', (t) => {
+  t.throws(() => {
+    harExtract({ outputDir: 'foo', inputFile: 'foo/bar.json' });
+  });
+});
+
+test('use default outputDir', t => harExtract({ inputFile: 'test/fixtures/sample-har.json' }).then(({ outputDir }) => {
+  t.is(outputDir, path.resolve('output'));
+
+  return promisedRimraf(outputDir);
+}));
 
 test('extract har file', (t) => {
   const tmpDir = uuid();
