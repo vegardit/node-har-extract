543cc3170bff9a208e6b6cc5c907d9cd10c9d069
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2019-07-26 23:27:01 +0200

    adds prettier config and formats

--- test/index.test.js
+++ test/index.test.js
@@ -9,34 +9,38 @@ import rimraf from 'rimraf';
 import { uuid } from '../lib/utils';
 import harExtract from '../lib';
 
-const promisedRimraf = dir => new Promise((resolve, reject) => {
-  rimraf(dir, (rimrafError) => {
-    if (rimrafError) {
-      reject(rimrafError);
-      return;
-    }
+const promisedRimraf = dir =>
+  new Promise((resolve, reject) => {
+    rimraf(dir, rimrafError => {
+      if (rimrafError) {
+        reject(rimrafError);
+        return;
+      }
 
-    resolve();
+      resolve();
+    });
   });
-});
 
-test('main export is a function', (t) => {
+test('main export is a function', t => {
   t.is(typeof harExtract, 'function', 'should be of type function');
 });
 
-test('inputFile located inside outputDir', (t) => {
+test('inputFile located inside outputDir', t => {
   t.throws(() => {
     harExtract({ outputDir: 'foo', inputFile: 'foo/bar.json' });
   });
 });
 
-test('use default outputDir', t => harExtract({ inputFile: 'test/fixtures/sample.har' }).then(({ outputDir }) => {
-  t.is(outputDir, path.resolve('output'));
+test('use default outputDir', t =>
+  harExtract({ inputFile: 'test/fixtures/sample.har' }).then(
+    ({ outputDir }) => {
+      t.is(outputDir, path.resolve('output'));
 
-  return promisedRimraf(outputDir);
-}));
+      return promisedRimraf(outputDir);
+    },
+  ));
 
-test('extract har file', (t) => {
+test('extract har file', t => {
   const tmpDir = uuid();
 
   const extractionPromise = harExtract({
--- test/utils.test.js
+++ test/utils.test.js
@@ -6,15 +6,15 @@ import test from 'ava';
 // internal modules
 import utils from '../lib/utils';
 
-test('`uuid()` returns a string', (t) => {
+test('`uuid()` returns a string', t => {
   t.is(typeof utils.uuid(), 'string', 'should return value of type `string`');
 });
 
-test('`uuid()` returns unique value each time its invoked', (t) => {
+test('`uuid()` returns unique value each time its invoked', t => {
   t.not(utils.uuid(), utils.uuid(), 'values should be different');
 });
 
-test('`makeBufferFromContent()` returns Buffer instance from content, defaults to utf8 encoding', (t) => {
+test('`makeBufferFromContent()` returns Buffer instance from content, defaults to utf8 encoding', t => {
   const content = { text: 'foo' };
   const buf = utils.makeBufferFromContent({ content });
 
@@ -22,26 +22,33 @@ test('`makeBufferFromContent()` returns Buffer instance from content, defaults t
   t.is(buf.toString('utf8'), content.text, 'should default to utf8 encoding');
 });
 
-test('`makeBufferFromContent()` returns Buffer instance from base64 encoded content', (t) => {
+test('`makeBufferFromContent()` returns Buffer instance from base64 encoded content', t => {
   const plainText = 'foo';
   const encoding = 'base64';
-  const content = { text: Buffer.from(plainText, 'utf8').toString(encoding), encoding };
+  const content = {
+    text: Buffer.from(plainText, 'utf8').toString(encoding),
+    encoding,
+  };
   const buf = utils.makeBufferFromContent({ content });
 
   t.true(Buffer.isBuffer(buf), 'return value should be a buffer');
-  t.is(buf.toString('utf8'), plainText, 'should decode to original input value');
+  t.is(
+    buf.toString('utf8'),
+    plainText,
+    'should decode to original input value',
+  );
 });
 
-test('`makeResponseBodyFilename()` returns a string', (t) => {
+test('`makeResponseBodyFilename()` returns a string', t => {
   const entry = {};
   t.is(
     typeof utils.makeResponseBodyFilename({ entry }),
     'string',
-    'should return value of type `string`'
+    'should return value of type `string`',
   );
 });
 
-test('`addFilenameToEntries()` adds `filename` prop to each entry', (t) => {
+test('`addFilenameToEntries()` adds `filename` prop to each entry', t => {
   const entriesCount = 2;
   t.plan(entriesCount * 2);
 
@@ -58,11 +65,11 @@ test('`addFilenameToEntries()` adds `filename` prop to each entry', (t) => {
 });
 
 test.todo(
-  "`writeEntryResponseBodiesToTargetDir()` writes contents of each entry's buffer to `targetDir`"
+  "`writeEntryResponseBodiesToTargetDir()` writes contents of each entry's buffer to `targetDir`",
 );
 
 // eslint-disable-next-line no-unused-vars
-test.skip('`readHARFromFile()` reads har content from file', (t) => {
+test.skip('`readHARFromFile()` reads har content from file', t => {
   // returns promise
   // rejects when file error
   // rejects when content is not valid har
@@ -70,20 +77,26 @@ test.skip('`readHARFromFile()` reads har content from file', (t) => {
   // can read binary buffers
 });
 
-test.todo('`splitResponseBodyFromEntry()` creates buffer with response content for each entry');
-
 test.todo(
-  '`splitResponseBodyFromEntries()` invokes `splitResponseBodyFromEntry()` for each log.entry.response'
+  '`splitResponseBodyFromEntry()` creates buffer with response content for each entry',
 );
 
 test.todo(
-  '`replaceResponseContentTextWithFilename()` clones entry and puts `filename` into clone.response.content.text'
+  '`splitResponseBodyFromEntries()` invokes `splitResponseBodyFromEntry()` for each log.entry.response',
 );
 
 test.todo(
-  '`replaceResponseContentTextWithFilenames()` incokes `replaceResponseContentTextWithFilename()` for each item'
+  '`replaceResponseContentTextWithFilename()` clones entry and puts `filename` into clone.response.content.text',
 );
 
-test.todo('`cloneHARAndReplaceEntries()` clones har object and puts replaces entries in clone');
+test.todo(
+  '`replaceResponseContentTextWithFilenames()` incokes `replaceResponseContentTextWithFilename()` for each item',
+);
 
-test.todo('`writeHARToFile()` stringifies har object and writes the contents to file');
+test.todo(
+  '`cloneHARAndReplaceEntries()` clones har object and puts replaces entries in clone',
+);
+
+test.todo(
+  '`writeHARToFile()` stringifies har object and writes the contents to file',
+);
